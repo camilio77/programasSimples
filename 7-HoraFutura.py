d92555c65ca449c88bd5a97d072662277c89b542
camilio77 <156365533+camilio77@users.noreply.github.com>
2024-01-22 13:06:22 -0500

    codigosEjercicios

--- 7-HoraFutura.py
+++ 7-HoraFutura.py
@@ -1,10 +1,10 @@
 #calcular la hora dentro de un numero especifico de horas
 ho = float (input("ingrese la hora actual: \n"))
-de = float (input("ingrese el numero de horas: \n"))
-ht = (ho + de) % 24
+des = float (input("ingrese el numero de horas: \n"))
+ht = (ho + des)
 if(ht > 12):
     m = "pm"
     ht = ht - 12
 else:
     m = "am"
-print(f"dentro de {round(de)} horas el reloj marcara las {round(ht,2)} {m}")
+print(f"dentro de {round(des)} horas el reloj marcara las {round(ht,2)} {m}")
